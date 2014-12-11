98d8aa388368c5244151f2e15d3a3b3932c6b41d
Sander-VdB <mr.rednas@gmail.com>
2014-12-11 11:06:58 +0100

    eerste commit

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -14,6 +14,6 @@ public class GitCursus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hallo");
+        // TODO code application logic here
     }
 }
