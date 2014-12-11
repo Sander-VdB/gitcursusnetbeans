e3e22ddce79a9e27cec5afb55834e43f1921748c
Sander-VdB <mr.rednas@gmail.com>
2014-12-11 11:18:02 +0100

    hallo allemaal

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -14,6 +14,6 @@ public class GitCursus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hallo iedereen");
+        System.out.println("hallo allemaal");
     }
 }
